3bbb8d0e95e774f64cb6382c7407075778827d9b
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2026-03-16 20:12:09 +0700

    Menambahkan entity Ktp sesuai struktur tabel database

---
+++ src/main/java/com/deploy/praktikum3/model/entity/Ktp.java
@@ -0,0 +1,31 @@
+package com.deploy.praktikum3.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "ktp")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Ktp {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "nomor_ktp", unique = true)
+    private String nomorKtp;
+
+    @Column(name = "nama_lengkap")
+    private String namaLengkap;
+
+    private String alamat;
+
+    @Column(name = "tanggal_lahir")
+    private Date tanggalLahir;
+
+    @Column(name = "jenis_kelamin")
+    private String jenisKelamin;
+}
